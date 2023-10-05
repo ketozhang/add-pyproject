3f77268f7be201b3feaa0b0b13ec1dfc698f9a2f
Keto D. Zhang <ketoz@ipac.caltech.edu>
2023-10-04 19:33:51 -0700

    Add CLI

--- add_pyproject/__main__.py
+++ add_pyproject/__main__.py
@@ -1,10 +1,13 @@
 # SPDX-FileCopyrightText: 2023-present Keto D. Zhang <ketozhang@gmail.com>
 #
 # SPDX-License-Identifier: MIT
-import sys
+
 
 if __name__ == "__main__":
-    from add_pyproject.cli import add_pyproject
+    import sys
 
-    args = sys.argv[1:]
-    sys.exit(add_pyproject(args[0]))
+    import add_pyproject
+
+    add_pyproject.main(sys.argv[1:])
+
+
